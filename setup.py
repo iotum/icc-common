713bf81f37382901af52d03ff1680c4647766754
Simon Ditner <simon@iotum.com>
2015-01-08 15:07:30 +0000

    dbwrapper is common between two of the projects, so spin it out

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name = 'icc-common-library',
+    version = '1.0',
+    description = 'Common ICC library',
+    author = 'Simon P. Ditner',
+    author_email = 'simon@iotum.com',
+    packages = ['icc'],
+)
+
