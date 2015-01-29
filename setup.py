9f3c895c5f3bd2e4eb79cbf2f1cb4f5425d8c943
Simon Ditner <simon@iotum.com>
2015-01-29 08:55:32 -0800

    Increment version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = 'icc-common-library',
-    version = '1.0',
+    version = '1.0.1',
     description = 'Common ICC library',
     author = 'Simon P. Ditner',
     author_email = 'simon@iotum.com',
