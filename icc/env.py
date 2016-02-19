86f27e458e01b65736a4c42dcfdc697b2333cbd7
Simon Ditner <simon@iotum.com>
2016-02-19 15:20:01 +0000

    Surprising number of mistakes in 10 lines

--- icc/env.py
+++ icc/env.py
@@ -3,9 +3,9 @@ import os
 
 def get(key, default=None):
     """ Convert true/false strings into python boolean """
-    val = os.getenv('key')
+    val = os.getenv(key)
     if val and val.lower() == 'true':
         val = True
-    elif val and val.ower() == 'false':
+    elif val and val.lower() == 'false':
         val = False
     return val
