6efa2de975f404476a7ec397b1a9bb1956706800
jerry_hu <jerry@ptellp.com>
2017-06-21 17:17:26 -0400

    incr version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='icc-common-library',
-    version='1.0.4',
+    version='1.0.5',
     description='Common ICC library',
     author='Simon P. Ditner',
     author_email='simon@iotum.com',
